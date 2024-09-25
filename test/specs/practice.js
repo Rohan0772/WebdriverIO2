cc88a11eaa96b608753e87a68dd5d0848bd265a2
rohan <rohan91455@gmail.com>
2024-09-25 17:58:02 +0530

    Added new changes and files

--- test/specs/practice.js
+++ test/specs/practice.js
@@ -2,4 +2,5 @@ let arr = [1, 2, 3,,2,2,2, 4, 5, 2, 3];
 let duplicates = arr.filter((item, index) => arr.indexOf(item) !== index);
 
 console.log([...new Set(duplicates)]); // Output: [2, 3]
-//console.log(duplicates);
+console.log(duplicates);
+
